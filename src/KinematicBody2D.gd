fd0b1f80897e9b32887d8789f5906328a085093e
jonnyhappyfeet <55300110+jonnyhappyfeet@users.noreply.github.com>
2021-03-12 16:20:26 -0600

    update

--- src/KinematicBody2D.gd
+++ src/KinematicBody2D.gd
@@ -1,5 +1,12 @@
 extends KinematicBody2D
 
+#constants defined
+const UP = Vector2(0, -1)
+const GRAVITY = 20
+const SPEED = 30
+const JUMP_HEIGHT = -550
+
+#variables defined
 var motion = Vector2()
 
 # Called when the node enters the scene tree for the first time.
@@ -7,14 +14,20 @@ var motion = Vector2()
 #	pass # Replace with function body.
 
 func _physics_process(delta):
-	motion.y += 10
+	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x += 100
+		motion.x += SPEED
 	elif Input.is_action_pressed("ui_left"):
-		motion.x += -100
+		motion.x += -SPEED
+	else:
+		motion.x = 0;
 	
-	move_and_slide(motion)
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+	
+	move_and_slide(motion, UP)
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
